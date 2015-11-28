eb2f0fd919594cf120eb65658e9aa2fdc1390b8a
Raphael Pantaleão <raphaelpanta@gmail.com>
2015-11-28 13:10:51 -0300

    Componente description-card alterado para ficar mais genérico.

--- src/app/components/description-card/description-card.js
+++ src/app/components/description-card/description-card.js
@@ -8,18 +8,30 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require("angular2/angular2");
+var models_1 = require("../commons/models");
 var GdgDescriptionCard = (function () {
     function GdgDescriptionCard() {
+        this.model = new DescriptionModel("Título", "Descrição", new models_1.LinkModel("Leia mais", "#"), [new models_1.MenuItemModel("Leia mais", "#")]);
     }
     GdgDescriptionCard = __decorate([
         angular2_1.Component({
             selector: "description-card"
         }),
         angular2_1.View({
-            templateUrl: "./app/components/description-card/description-card.html"
+            templateUrl: "./app/components/description-card/description-card.html",
+            directives: [angular2_1.NgFor]
         }), 
         __metadata('design:paramtypes', [])
     ], GdgDescriptionCard);
     return GdgDescriptionCard;
 })();
 exports.GdgDescriptionCard = GdgDescriptionCard;
+var DescriptionModel = (function () {
+    function DescriptionModel(title, descriptionText, link, menu) {
+        this.title = title,
+            this.descriptionText = descriptionText,
+            this.link = link,
+            this.menu = menu;
+    }
+    return DescriptionModel;
+})();
--- src/app/components/description-card/description-card.ts
+++ src/app/components/description-card/description-card.ts
@@ -1,9 +1,38 @@
-import {Component, View} from "angular2/angular2";
+import {Component, View, NgFor} from "angular2/angular2";
+
+import {LinkModel, MenuItemModel} from "../commons/models";
 
 @Component({
   selector: "description-card"
 })
 @View({
-  templateUrl: "./app/components/description-card/description-card.html"
+  templateUrl: "./app/components/description-card/description-card.html",
+  directives: [NgFor]
 })
-export class GdgDescriptionCard{}
+export class GdgDescriptionCard{
+  model = new DescriptionModel(
+    "Título",
+    "Descrição",
+    new LinkModel("Leia mais", "#"),
+    [new MenuItemModel("Leia mais","#")]
+  );
+}
+
+class DescriptionModel {
+  title: String;
+  descriptionText: String;
+  link: LinkModel;
+  menu: MenuItemModel[];
+
+  constructor(
+    title: String,
+    descriptionText: String,
+    link: LinkModel,
+    menu: MenuItemModel[]
+  ) {
+    this.title = title,
+    this.descriptionText = descriptionText,
+    this.link = link,
+    this.menu = menu
+  }
+}
